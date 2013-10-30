9e93b7776b0823955e58ca581910fcb492ea626a
logan <logan@Latitude.(none)>
2013-10-30 16:59:24 +0000

    removed Test.java

--- src/Test.java
+++
@@ -1,9 +0,0 @@
-/**
- * Created with IntelliJ IDEA.
- * User: logan
- * Date: 10/30/13
- * Time: 4:56 PM
- * To change this template use File | Settings | File Templates.
- */
-public class Test {
-}
