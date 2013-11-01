8813a95b68aae2ae285f75997991fafda5024407
logan <logan@Latitude.(none)>
2013-11-01 15:04:19 +0000

    parse car setup and set cars

--- src/com/example/TrafficJam/PlayActivity.java
+++ src/com/example/TrafficJam/PlayActivity.java
@@ -2,6 +2,10 @@ package com.example.TrafficJam;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created with IntelliJ IDEA.
@@ -12,10 +16,70 @@ import android.os.Bundle;
  */
 public class PlayActivity extends Activity {
     PlayView  m_gv;
+	List<Car> cars = new ArrayList<Car>();
     public void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
+
+	    Bundle b = getIntent().getExtras();
+	    String setup = b.getString("setup");
+
+	    Toast.makeText(getApplicationContext(), "Puzzle: " + "  ListItem : " + setup, Toast.LENGTH_LONG).show();
+
+	    parseSetup(setup);
+
         setContentView(R.layout.play);
-        m_gv = (PlayView) findViewById( R.id.playview);
+        m_gv = (PlayView) findViewById( R.id.play_view);
+	    m_gv.setCars(cars);
     }
+
+	public void parseSetup(String setup)
+	{
+		//(H 1 2 2), (V 0 1 3), (H 0 0 2), (V 3 1 3), (H 2 5 3), (V 0 4 2), (H 4 4 2), (V 5 0 3)
+		//setBoard("HH...VV..V.vvHHv.VV..V..V...HHV.HhH.");
+		Car car = null;
+		int count = 0;
+		boolean newCar = false;
+
+		for(int i=0; i<setup.length(); i++)
+		{
+			if(setup.charAt(i) == '(') //new car
+			{
+				car = new Car();
+				newCar = true;
+			}
+			if(newCar)
+			{
+				if(setup.charAt(i) == 'H')
+					car.alignment = 'H';
+				if(setup.charAt(i) == 'V')
+					car.alignment = 'V';
+
+				if(Character.isDigit(setup.charAt(i)))
+				{
+					if(count == 0)
+					{
+						car.x = Integer.parseInt(String.valueOf(setup.charAt(i)));
+						count++;
+					}
+					else if(count == 1)
+					{
+						car.y = Integer.parseInt(String.valueOf(setup.charAt(i)));
+						count++;
+					}
+					else if(count == 2)
+					{
+						car.length = Integer.parseInt(String.valueOf(setup.charAt(i)));
+						count++;
+					}
+				}
+			}
+			if(setup.charAt(i) == ')')
+			{
+				cars.add(car);
+				newCar = false;
+				count = 0;
+			}
+		}
+	}
 }
